3214a371360e0a6f30559e2397da9a8a1c371af0
J0bot <jojo64364@gmail.com>
2022-03-17 23:32:38 +0100

    add icon

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="https://yt3.ggpht.com/yti/APfAmoHmEJMspUtHkNBkTy4K0Riiv8O3vVZXXfSsZWeUwQ=s88-c-k-c0x00ffffff-no-rj-mo">
     <title>J0bot's website</title>
 </head>
 <body>

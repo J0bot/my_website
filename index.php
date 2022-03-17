4beca8d7837aa5d91105c296b599a0e65d452270
J0bot <jojo64364@gmail.com>
2022-03-17 23:28:01 +0100

    tiny changes

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>J0bot's website</title>
+</head>
+<body>
+    
+    
 <?php 
 echo "hello world"; 
 ?>
 
 <script>alert("Get out of this site")</script>
+
+</body>
+</html>

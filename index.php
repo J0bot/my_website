faaaa8dd041f56303d20481664cb0fe6eb2c4aab
Josécarlos Gasser <px01evl@etmlnet.local>
2022-03-24 10:21:09 +0100

    test

--- index.php
+++ index.php
@@ -4,15 +4,21 @@
     <meta charset="UTF-8">
     <link rel="icon" href="https://yt3.ggpht.com/yti/APfAmoHmEJMspUtHkNBkTy4K0Riiv8O3vVZXXfSsZWeUwQ=s88-c-k-c0x00ffffff-no-rj-mo">
     <title>J0bot's website</title>
+    <script src="https://aframe.io/releases/1.3.0/aframe.min.js"></script>
 </head>
 <body>
     
     
-<?php 
-echo "hello world"; 
-?>
 
 <script>alert("Get out of this site, NOW")</script>
+    <a-scene>
+      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+      <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+      <a-sky color="#ECECEC"></a-sky>
+    </a-scene>
+
 
 </body>
 </html>

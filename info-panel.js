8887cad4e5210089470168085129f8b22bc61b93
Jobot <jojo64364@gmail.com>
2022-03-29 20:22:21 +0200

    page test

--- info-panel.js
+++ info-panel.js
@@ -49,10 +49,12 @@ AFRAME.registerComponent('info-panel', {
     else if(movieTitle=="CV")
     {
       alert("pas encore finit");
+      
     }
     else if(movieTitle=="TESTS")
     {
       alert("Il n'y a pas de tests");
+      window.location.href = "http://j0bot.ch/?page=test";
     }
 
   },
